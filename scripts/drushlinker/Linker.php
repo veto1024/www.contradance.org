f4eaecc492d42ec6e31d21fe71c6589936838dd5
veto1024 <veto1024@gmail.com>
2019-12-21 22:08:00 -0800

    Drush autolinker commit

--- scripts/drushlinker/Linker.php
+++ scripts/drushlinker/Linker.php
@@ -10,19 +10,24 @@ namespace DrushLinker;
 
 use Composer\Script\Event;
 
-class Linker
-{
+class Linker {
+
+  /*
+   * linkDrush function
+   */
+
   public static function linkDrush(Event $event) {
-    $target_pointer = $drupalRoot."/vendor/bin/drush";
+    $target_pointer = $drupalRoot . "/vendor/bin/drush";
     $link_name = "/usr/bin/drush";
-    $result= symlink($target_pointer, $link_name);
-    if ($result)
-    {
-      $event->getIO()->write("Symlink to Drush created!");
-    }
-    else
-    {
-      $event->getIO()->write("Symlink to Drush NOT created!");
+    if (!is_link($link_name)) {
+      $result = symlink($target_pointer, $link_name);
+      if ($result) {
+        $event->getIO()->write("Symlink to Drush created!");
+      }
+      else {
+        $event->getIO()->write("Symlink to Drush NOT created!");
+      }
     }
   }
+
 }
