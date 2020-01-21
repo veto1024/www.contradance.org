319ef8953d60066178fee7ec4130223f79036adf
veto1024 <veto1024@gmail.com>
2020-01-20 21:47:02 -0500

    Update: CSS Added: ccd_financials module for including revenue summary on user pages

---
+++ web/modules/custom/ccd_financials/src/Plugin/Block/YTDRevenueBlock.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\ccd_financials\Plugin\Block\DonationButtonBlock.
+ */
+namespace Drupal\ccd_financials\Plugin\Block;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Block\Annotation\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+
+/**
+ * Provides an 'ytd_revenue_block' block.
+ *
+ * @Block(
+ *   id = "ccd_financials_ytd_revenue_block",
+ *   admin_label = @Translation("YTD Revenue Block"),
+ *   category = @Translation("Block showing CCD Year To Date Info")
+ * )
+ */
+class YTDRevenueBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'access content');
+  }
+
+  public function blockForm($form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+
+    return $form;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function build() {
+
+    $query = \Drupal::entityQuery('node')
+      ->condition('type','event')
+      ->condition('status',1)
+      ->condition('field_event_date.value', array("2020-01-01T00:00:00", "2021-01-01T00:00:00"), 'BETWEEN');
+
+    $results=$query->execute();
+    $total_rev = 0.0;
+    $total_exp = 0.0;
+    foreach ($results as $result) {
+      $node = Node::load($result);
+      $revenue = $node->get('field_gross_revenue')[0];
+      $total_rev += $revenue->value;
+      $expenses = $node->get('field_band_pay')[0]->value +
+        $node->get('field_band_travel')[0]->value +
+        $node->get('field_caller_pay')[0]->value +
+        $node->get('field_caller_travel')[0]->value +
+        $node->get('field_rent_cost')[0]->value +
+        $node->get('field_sound_pay')[0]->value +
+        $node->get('field_other_expenses')[0]->value;
+      $total_exp += $expenses;
+    }
+    return [
+      '#theme' => 'ccd_financials_ytd_revenue',
+      '#total_rev' => $total_rev,
+      '#total_exp' => $total_exp,
+      '#net_profit' => $total_rev - $total_exp,
+    ];
+  }
+
+}
