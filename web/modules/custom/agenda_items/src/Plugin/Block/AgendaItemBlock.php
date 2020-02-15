302e80c7c33c97e649157f9fab6dde40ec978d2f
veto1024 <veto1024@gmail.com>
2020-02-15 15:53:10 -0500

    Drupal files creates private directory now Added logic to disable agenda addition block is meeting closed to items.

--- web/modules/custom/agenda_items/src/Plugin/Block/AgendaItemBlock.php
+++ web/modules/custom/agenda_items/src/Plugin/Block/AgendaItemBlock.php
@@ -5,6 +5,8 @@
  */
 namespace Drupal\agenda_items\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\node\Entity\Node;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Provides an 'agenda_items' block.
@@ -19,8 +21,26 @@ class AgendaItemBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
+
   public function build() {
+    if ($this->isVisible()){
     $form = \Drupal::formBuilder()->getForm('\Drupal\agenda_items\Form\AgendaForm');
+    }
     return $form;
   }
+
+  protected function isVisible () {
+    $nid = \Drupal::routeMatch()->getParameter('node')->id();
+    $node = Node::load($nid);
+    $status = $node->get('field_meeting_status');
+    $tid = $status->getValue()[0]['target_id'];
+    $term = Term::load($tid);
+    if ($term->get('name')->value == 'Collecting agenda items') {
+      return TRUE;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
 }
